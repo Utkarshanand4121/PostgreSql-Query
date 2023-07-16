612a16baecfc34c507358a1e3b2467a8a3294605
Utkarshanand4121 <129711901+Utkarshanand4121@users.noreply.github.com>
2023-07-16 22:59:44 +0530

    Update HakerRank Query.sql

--- HakerRank Query.sql
+++ HakerRank Query.sql
@@ -149,6 +149,7 @@ select round(sqrt(power(max(lat_n)-min(lat_n),2)+power(max(long_w)-min(long_w),2
 from station;
 
 --African Cities--
+
 select city.name from city
 left join country on city.countrycode=country.code
 where country.continent='Africa';
