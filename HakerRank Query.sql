3719500ab8ecdebdd5f7c9ca8c3e47a753615693
UTKARSH ANAND <129711901+Utkarshanand4121@users.noreply.github.com>
2023-08-02 20:51:11 +0530

    Update HakerRank Query.sql

--- HakerRank Query.sql
+++ HakerRank Query.sql
@@ -1,24 +1,42 @@
+-- Revising the select Query 1--
+
 select * from CITY where population>100000 and CountryCode='USA';
+
+-- Revising the select Query 2--
 
 select name from CITY where population>120000 and CountryCode='USA';
 
+--Select All--
+
 select * from city;
+
+--Select by id--
 
 select * from city where id=1661;
 
+--Japanese City Attributes--
+
 select * from city where countrycode='JPN';
+
+--Japanese City Names--
 
 select name from city where countrycode='JPN';
 
+--Weather Observation station 1--
+
 select city,state from station;
+
+--Weather Observation station 3--
 
 select distinct city from station where id % 2=0;
 
+--Weather Observation station 4--
 select count(city)-count(distinct city) from station;
 
+--Weather Observation station 6--
 select distinct city from station where city like'%A' or city like'%E' or city like'%I' or city like'%O' or city like'%U'; 
 
--- Weather Observation Station8 Problem--
+-- Weather Observation Station 8 --
 select distinct city from station
 where (city like 'a%'
 or city like 'e%'
